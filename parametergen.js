cb65fef6d03551535f063f1e41b5943b61c468af
Bacorn42 <kacper123987@gmail.com>
2019-06-06 04:40:26 +0200

    Made the code slightly nicer.

--- parametergen.js
+++ parametergen.js
@@ -9,51 +9,30 @@ function exportData() {
 function generateParameters() {
     // Initial parameters
     const params = {
-        purchaseThreshold: Math.max(normal(10, 2.5), 0),
-        housePurchaseThreshold: Math.max(normal(10, 2.5), 0),
-        bidUp: Math.max(normal(30, 6), 0),
+        purchaseThreshold: normal(10, 2.5),
+        housePurchaseThreshold: normal(10, 2.5),
+        bidUp: normal(30, 6),
         tradeAcceptanceThreshold: normal(0, 10),
         tradeRejectionThreshold: normal(-100, 20),
-        spotsBehind: Math.floor(Math.min(Math.max(normal(10, 2.5), 0), 40)),
-        spotsAhead: Math.floor(Math.min(Math.max(normal(10, 2.5), 0), 40)),
-        communityChestJailCardValue: Math.max(normal(10, 2.5), 0),
-        chanceJailCardValue: Math.max(normal(10, 2.5), 0),
-        maxAmountOfRejections: Math.max(normal(3, 1), 1),
-        firstBuildingOdds: Math.min(Math.max(normal(35, 10), 0), 100),
-        lastBuildingOdds: Math.min(Math.max(normal(80, 10), 0), 100)
+        spotsBehind: normal(10, 2.5),
+        spotsAhead: normal(10, 2.5),
+        communityChestJailCardValue: normal(10, 2.5),
+        chanceJailCardValue: normal(10, 2.5),
+        maxAmountOfRejections: normal(3, 1),
+        firstBuildingOdds: normal(35, 10),
+        lastBuildingOdds: normal(80, 10)
     };
     const str = localStorage.getItem('gameHistory');
     const gameHistory = str ? JSON.parse(str) : [];
     // 20% chance of making player from initial parameters
     if(Math.random() < 0.8 && gameHistory.length > 1) {
-        let newParamsMeans = {
-            purchaseThreshold: 0,
-            housePurchaseThreshold: 0,
-            bidUp: 0,
-            tradeAcceptanceThreshold: 0,
-            tradeRejectionThreshold: 0,
-            spotsBehind: 0,
-            spotsAhead: 0,
-            communityChestJailCardValue: 0,
-            chanceJailCardValue: 0,
-            maxAmountOfRejections: 0,
-            firstBuildingOdds: 0,
-            lastBuildingOdds: 0
-        };
-        let newParamsSDs = {
-            purchaseThreshold: 0,
-            housePurchaseThreshold: 0,
-            bidUp: 0,
-            tradeAcceptanceThreshold: 0,
-            tradeRejectionThreshold: 0,
-            spotsBehind: 0,
-            spotsAhead: 0,
-            communityChestJailCardValue: 0,
-            chanceJailCardValue: 0,
-            maxAmountOfRejections: 0,
-            firstBuildingOdds: 0,
-            lastBuildingOdds: 0
-        };
+        let newParamsMeans = {};
+        let newParamsSDs = {};
+        for(const param in params) {
+            newParamsMeans[param] = 0;
+            newParamsSDs[param] = 0;
+        }
+        
         let winningPlayers = [];
         for(const game of gameHistory) {
             for(const player of game) {
@@ -79,21 +58,20 @@ function generateParameters() {
             params[param] = normal(newParamsMeans[param], newParamsSDs[param]);
         }
         
-        params.purchaseThreshold = Math.max(params.purchaseThreshold, 0);
-        params.housePurchaseThreshold = Math.max(params.housePurchaseThreshold, 0);
-        params.bidUp = Math.max(params.bidUp, 0);
-        params.spotsBehind = Math.floor(Math.min(Math.max(params.spotsBehind, 0), 40));
-        params.spotsAhead = Math.floor(Math.min(Math.max(params.spotsAhead, 0), 40));
-        params.communityChestJailCardValue = Math.max(params.communityChestJailCardValue, 0);
-        params.chanceJailCardValue = Math.max(params.chanceJailCardValue, 0);
-        params.maxAmountOfRejections = Math.max(params.maxAmountOfRejections, 1);
-        params.firstBuildingOdds = Math.min(Math.max(params.firstBuildingOdds, 0), 100);
-        params.lastBuildingOdds = Math.min(Math.max(params.lastBuildingOdds, 0), 100);
-        
         console.log('%c Created player from previous winners.', 'color: green;');
     } else {
         console.log('%c Created initial player.', 'color: red;');
     }
+    params.purchaseThreshold = Math.max(params.purchaseThreshold, 0);
+    params.housePurchaseThreshold = Math.max(params.housePurchaseThreshold, 0);
+    params.bidUp = Math.max(params.bidUp, 0);
+    params.spotsBehind = Math.floor(Math.min(Math.max(params.spotsBehind, 0), 40));
+    params.spotsAhead = Math.floor(Math.min(Math.max(params.spotsAhead, 0), 40));
+    params.communityChestJailCardValue = Math.max(params.communityChestJailCardValue, 0);
+    params.chanceJailCardValue = Math.max(params.chanceJailCardValue, 0);
+    params.maxAmountOfRejections = Math.max(params.maxAmountOfRejections, 1);
+    params.firstBuildingOdds = Math.min(Math.max(params.firstBuildingOdds, 0), 100);
+    params.lastBuildingOdds = Math.min(Math.max(params.lastBuildingOdds, 0), 100);
     console.log(params);
     
     return params;
